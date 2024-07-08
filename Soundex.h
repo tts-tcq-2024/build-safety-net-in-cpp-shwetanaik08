259fda1ff1b16b4b0c4d8963d2da570e69151373
shwetanaik08 <112984589+shwetanaik08@users.noreply.github.com>
2024-07-08 14:30:38 +0530

    Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -3,6 +3,15 @@
 
 #include <string>
 
+// Function to get Soundex code for a character
+char getSoundexCode(char c);
+
+// Function to remove adjacent duplicates
+std::string removeAdjacentDuplicates(const std::string& str);
+
+// Function to pad the string to a length of 4
+std::string padToLength(const std::string& str, size_t length);
+
 std::string generateSoundex(const std::string& name);
 
 #endif // SOUNDEX_H
