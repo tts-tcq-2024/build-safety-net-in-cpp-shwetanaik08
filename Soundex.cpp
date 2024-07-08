bf61ab8ccfdcc9d982937f8c5c0cf8479d2e8650
shwetanaik08 <112984589+shwetanaik08@users.noreply.github.com>
2024-07-08 14:12:43 +0530

    First commit

--- Soundex.cpp
+++ Soundex.cpp
@@ -1,9 +1,9 @@
-#include "Soundex.h"
 #include <cctype>
+#include <string>
 
+// Function to get Soundex code for a character
 char getSoundexCode(char c) {
-    c = toupper(c);
-    switch (c) {
+    switch (toupper(c)) {
         case 'B': case 'F': case 'P': case 'V': return '1';
         case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
         case 'D': case 'T': return '3';
@@ -14,23 +14,48 @@ char getSoundexCode(char c) {
     }
 }
 
-std::string generateSoundex(const std::string& name) {
-    if (name.empty()) return "";
+// Function to remove adjacent duplicates
+std::string removeAdjacentDuplicates(const std::string& str) {
+    if (str.empty()) return "";
 
-    std::string soundex(1, toupper(name[0]));
-    char prevCode = getSoundexCode(name[0]);
+    std::string result;
+    result += str[0];
 
-    for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
-        char code = getSoundexCode(name[i]);
-        if (code != '0' && code != prevCode) {
-            soundex += code;
-            prevCode = code;
+    for (size_t i = 1; i < str.size(); ++i) {
+        if (str[i] != str[i - 1]) {
+            result += str[i];
         }
     }
 
-    while (soundex.length() < 4) {
-        soundex += '0';
+    return result;
+}
+
+// Function to pad the string to a length of 4
+std::string padToLength(const std::string& str, size_t length) {
+    std::string result = str;
+    while (result.size() < length) {
+        result += '0';
+    }
+    return result.substr(0, length);
+}
+
+// Function to generate Soundex code
+std::string generateSoundex(const std::string& name) {
+    if (name.empty()) return "";
+
+    std::string soundex;
+    soundex += toupper(name[0]);
+
+    std::string codes;
+    for (size_t i = 1; i < name.size(); ++i) {
+        char code = getSoundexCode(name[i]);
+        if (code != '0') {
+            codes += code;
+        }
     }
 
-    return soundex;
+    codes = removeAdjacentDuplicates(codes);
+    soundex += codes;
+
+    return padToLength(soundex, 4);
 }
